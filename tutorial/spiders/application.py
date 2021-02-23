eaf1d60a8aa22fb8c553d0c8dfebceac5b787400
Sergiu Ster <ssterdev@gmail.com>
2021-02-23 19:50:40 +0200

    bulk add

--- tutorial/spiders/application.py
+++ tutorial/spiders/application.py
@@ -1,22 +1,89 @@
-from flask import Flask, render_template
-import sqlite3
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3,csv
 
-DATABASE='main.db'
+DATABASE='test.db'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/sergiu/bettinganalyzer/tutorial/spiders/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Meci(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(15), nullable=False)
+    data = db.Column(db.String(20), nullable=False)
+    home_team = db.Column(db.String(50), nullable=False)
+    away_team = db.Column(db.String(50), nullable=False)
+    home_Fresult = db.Column(db.Integer(), nullable=True)
+    away_Fresult = db.Column(db.Integer(), nullable=True)
+    home_HTresult = db.Column(db.Integer(), nullable=True)
+    away_HTresult = db.Column(db.Integer(), nullable=True)
+
+    def __repr__(self):
+        return '<Meci %r>' % self.id
+
+    def create(self, status, data, home_team, away_team, home_Fresult, away_Fresult, home_HTresult, away_HTresult):
+        self.status = status
+        self.data = data
+        self.home_team = home_team
+        self.away_team = away_team
+        self.home_Fresult = home_Fresult
+        self.away_Fresult = away_Fresult
+        self.home_HTresult = home_HTresult
+        self.away_HTresult = away_HTresult
+        
 
 @app.route('/')
 
 def get_db():
-	db = getattr(app, '_database', None)
-	if db is None: 
-		db = app._database = sqlite3.connect(DATABASE)
-	cursor = db.execute("SELECT * from matches")
-	rows = cursor.fetchall()
+    db = getattr(app, '_database', None)
+    if db is None: 
+        db = app._database = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    cursor = db.execute("SELECT * from meci")
+    rows = cursor.fetchall()
 
-	return render_template('index.html', object = rows)
-	
+    return render_template('index.html', echipa = rows)
 
+def read_csv():
+    meci_nou = Meci()
+    with open('items.csv', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            meci_nou.create(row['status'],row['data'],row['team_1'],row['team_2'],row['final_score_home'],row['final_score_away'],row['first_half_home'],row['first_half_away'])
+            db.session.add(meci_nou)
+            db.session.commit()
+
+
+@app.route('/test')
+
+def team_form():
+    echipa = request.args.get('echipa')
+    home = 0;
+    away = 0;
+    draw = 0;
+    db = getattr(app, '_database', None)
+    if db is None:
+        db = app._database = sqlite3.connect(DATABASE)
+
+    db.row_factory = sqlite3.Row
+    cursor=db.execute('SELECT * from meci WHERE home_team LIKE "%{val1}%" OR away_team LIKE "%{val1}%" GROUP BY data having status LIKE "%Finished%" ORDER BY data DESC LIMIT 10;'.format(val1=echipa))
+    rows = cursor.fetchall()
+
+    return render_template('index.html', homewin = home, awaywin = away, draw = draw, echipa=rows)
+
+@app.route('/calculate')
+
+def calculate():
+    db = getattr(app, '_database', None)
+    if db is None:
+        db = app._database = sqlite3.connect(DATABASE)
+
+    db.row_factory = sqlite3.Row
+    cursor=db.execute('SELECT  win_status, COUNT(*) FROM(SELECT * from meci WHERE home_team LIKE "%Bologna%" OR away_team LIKE "%Bologna%" ORDER BY status ASC,data DESC LIMIT 10)GROUP BY win_status;')
+    row = cursor.fetchall()
+    return render_template("calculate.html", stat = row) 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
--- tutorial/spiders/database_import.py
+++ tutorial/spiders/database_import.py
@@ -1,13 +1,13 @@
 import sqlite3
 import csv
 
-conn = sqlite3.connect('main.db')
+conn = sqlite3.connect('test.db')
 
 def create_db():
 	c = conn.cursor()
 	# Create table
 	c.execute('''CREATE TABLE matches
-				(status text, data text, team_1 text, team_2 text, final_score_home text, final_score_away text, first_half_home text, first_half_away text)''')
+				(status text, data text, team_1 text, team_2 text, final_score_home text, final_score_away text, first_half_home text, first_half_away text, win_status text)''')
 	conn.commit()
 
 def insert_db():
@@ -16,5 +16,12 @@ def insert_db():
 	with open('items.csv','r') as fin:
 		dr = csv.DictReader(fin)
 		to_db = [(i['status'], i['data'], i['team_1'], i['team_2'], i['final_score_home'], i['final_score_away'], i['first_half_home'], i['first_half_away']) for i in dr]
-	c.executemany("INSERT INTO matches (status, data, team_1, team_2, final_score_home, final_score_away, first_half_home, first_half_away) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", to_db)
+	c.executemany("INSERT INTO meci (status, data, home_team, away_team, home_Fresult, away_Fresult, home_HTresult, away_HTresult) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", to_db)
 	conn.commit()
+
+def define_winner():
+    c = conn.cursor()
+
+    c.execute("UPDATE matches SET win_status=CASE WHEN final_score_home>final_score_away THEN '1' WHEN final_score_home<final_score_away THEN '2' WHEN final_score_home=final_score_away THEN 'X' WHEN status LIKE '%Not started%' THEN 'i' END") 
+    conn.commit()
+    
